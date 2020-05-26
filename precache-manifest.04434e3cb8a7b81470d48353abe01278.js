5544ee123fd11e741aeaa60305a4df2892618f45
TauriKalve <tauri@digiway.ee>
2020-05-26 20:01:31 +0300

    Updates

---
+++ precache-manifest.04434e3cb8a7b81470d48353abe01278.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf60d05f470c133eaf1fe0f138b6b905",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "61cb64c24c3189a5ad14",
+    "url": "/monsters-rolodex/static/css/main.4a23888c.chunk.css"
+  },
+  {
+    "revision": "2e727d9017460540a5f7",
+    "url": "/monsters-rolodex/static/js/2.24e11aac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.24e11aac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61cb64c24c3189a5ad14",
+    "url": "/monsters-rolodex/static/js/main.566f315e.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
